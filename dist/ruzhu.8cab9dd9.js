47d6e21030ce67a182d985d281ee5c9298f02f85
minured <1296900813@qq.com>
2020-07-08 15:46:17 +0800

    update

--- dist/ruzhu.8cab9dd9.js
+++ dist/ruzhu.8cab9dd9.js
@@ -1925,7 +1925,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60581" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61303" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
