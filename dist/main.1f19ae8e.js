d59bacdbffa9401e46f66cafea709eb362928057
minured <minured@qq.com>
2020-07-10 14:49:58 +0800

    更新类目图标

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -21890,7 +21890,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53145" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49155" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
